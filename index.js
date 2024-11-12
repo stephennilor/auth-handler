f4c1a4a1fec2ae74486b05116715fbb1fd3652b3
stephennilor <152863586+stephennilor@users.noreply.github.com>
2024-11-12 04:30:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,26 @@ const port = process.env.PORT || 3000;
 app.get('/callback', (req, res) => {
   const code = req.query.code;
   
+  // Send a more robust response that ensures the window.opener exists
   res.send(`
-    <script>
-      if (window.opener) {
-        window.opener.postMessage({ code: '${code}' }, '*');
-        window.close();
-      }
-    </script>
+    <!DOCTYPE html>
+    <html>
+    <body>
+      <script>
+        try {
+          if (window.opener) {
+            window.opener.postMessage({type: 'SPOTIFY_AUTH', code: '${code}'}, '*');
+            window.close();
+          } else {
+            document.body.innerHTML = 'Please copy this code: ${code}';
+          }
+        } catch (e) {
+          document.body.innerHTML = 'Error: ' + e.message;
+        }
+      </script>
+      <p>Authentication successful! You can close this window.</p>
+    </body>
+    </html>
   `);
 });
 
